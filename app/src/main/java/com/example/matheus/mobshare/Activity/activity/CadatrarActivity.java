cf2428d6d55fecc77486c2e4e9aed856c6379875
MATHEUS VIEIRA COSTA <17259221@CFP127ALUNO.LOCAL>
2019-04-11 11:21:47 -0300

    Tela de login e cadastro prontas(Layout) - Matheus

---
+++ app/src/main/java/com/example/matheus/mobshare/Activity/activity/CadatrarActivity.java
@@ -0,0 +1,15 @@
+package com.example.matheus.mobshare.Activity.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.matheus.mobshare.R;
+
+public class CadatrarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadatrar);
+    }
+}
--- app/src/main/java/com/example/matheus/mobshare/Activity/activity/LoginActivity.java
+++ app/src/main/java/com/example/matheus/mobshare/Activity/activity/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.matheus.mobshare.Activity.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.animation.AnimationUtils;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 
@@ -13,16 +15,19 @@ import com.example.matheus.mobshare.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-
-    Animation fadeIn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
 
-        fadeIn = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.fadein);
+    public void cadastrar_conta(View v){
+        Intent intent = new Intent(this, CadatrarActivity.class);
+        startActivity(intent);
+    }
 
+    public void btnLogar(View v){
+        Log.e("OIIIIIIIIII", "FUNFANDO:");
     }
 
 }
