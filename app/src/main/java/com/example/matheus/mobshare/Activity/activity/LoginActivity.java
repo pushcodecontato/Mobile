1f4738e31d4fe9db6524a25bb8ad69e207587c20
MatheusVCosta <vieira.matheuscosta@gmail.com>
2019-04-11 18:55:36 -0300

    Implementação do Drawermenu(não testado) - Matheus

--- app/src/main/java/com/example/matheus/mobshare/Activity/activity/LoginActivity.java
+++ app/src/main/java/com/example/matheus/mobshare/Activity/activity/LoginActivity.java
@@ -2,14 +2,8 @@ package com.example.matheus.mobshare.Activity.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.animation.AnimationUtils;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
 
 import com.example.matheus.mobshare.R;
 
@@ -27,7 +21,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void btnLogar(View v){
-        Log.e("OIIIIIIIIII", "FUNFANDO:");
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 
 }
